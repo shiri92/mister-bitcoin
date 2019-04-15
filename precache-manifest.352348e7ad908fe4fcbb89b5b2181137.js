10b4f79a91e4a321795a87abf2b08264cab9e115
shiri92 <shiriron92@gmail.com>
2019-04-15 14:45:18 +0300

    Updates

---
+++ precache-manifest.352348e7ad908fe4fcbb89b5b2181137.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9590c752a00658eae37c",
+    "url": "/mister-bitcoin/static/js/runtime~main.173c7f6e.js"
+  },
+  {
+    "revision": "bb4ea1538ca07c66219e",
+    "url": "/mister-bitcoin/static/js/main.b688d589.chunk.js"
+  },
+  {
+    "revision": "b055042600654aa7f638",
+    "url": "/mister-bitcoin/static/js/2.026a5579.chunk.js"
+  },
+  {
+    "revision": "bb4ea1538ca07c66219e",
+    "url": "/mister-bitcoin/static/css/main.3cc92c6e.chunk.css"
+  },
+  {
+    "revision": "5505765a1a17ef7f737ea3771dc1522d",
+    "url": "/mister-bitcoin/index.html"
+  }
+];
